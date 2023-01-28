4913b787817085bf3e5e4cb23a21b8df3ebc314d
Brett Jia <dev.bjia56@gmail.com>
2023-01-28 00:28:50 -0500

    Remove concurrency for now

--- scripts/concurrent_wheels.sh
+++ scripts/concurrent_wheels.sh
@@ -13,16 +13,16 @@ repair_wheel() (
     auditwheel repair *armv7l.whl
 )
 
-build_wheel 3.7 &
-build_wheel 3.8 &
-build_wheel 3.9 &
-build_wheel 3.10 &
-build_wheel 3.11 &
-wait
+build_wheel 3.7
+build_wheel 3.8
+build_wheel 3.9
+build_wheel 3.10
+build_wheel 3.11
+#wait
 
-repair_wheel 3.7 &
-repair_wheel 3.8 &
-repair_wheel 3.9 &
-repair_wheel 3.10 &
-repair_wheel 3.11 &
-wait
+repair_wheel 3.7
+repair_wheel 3.8
+repair_wheel 3.9
+repair_wheel 3.10
+repair_wheel 3.11
+#wait
