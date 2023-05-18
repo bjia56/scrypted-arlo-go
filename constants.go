9b499c75c7989922d0fc508aa433116cbf7c38d7
Brett Jia <dev.bjia56@gmail.com>
2023-05-18 18:00:31 -0400

    add backup auth hosts (#6)
    * add backup auth hosts
    
    * remove macos-10.15 runner

---
+++ constants.go
@@ -0,0 +1,13 @@
+package scrypted_arlo_go
+
+var BACKUP_AUTH_HOSTS = []string{
+	"NTIuMTYuMTM4LjI=",
+	"NjMuMzIuODEuMjE4",
+	"MzQuMjQ2LjIzOS4xNzk=",
+	"NTQuNzMuMjI5LjUz",
+	"MzQuMjUwLjE4MC4xOTA=",
+	"MzQuMjU0LjIxNS4xOTQ=",
+	"MzQuMjQzLjkwLjIwMA==",
+	"NTQuNzIuMjI0LjI0Mg==",
+	"MzQuMjUyLjIyMS40Ng==",
+}
