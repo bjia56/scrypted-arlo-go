6ac83d0924f7f8f1b9ce65dc218d4b94033540b6
Brett Jia <dev.bjia56@gmail.com>
2023-06-01 23:00:59 -0400

    add function to verify certs (#14)

--- scrypted_arlo_go/setup.py
+++ scrypted_arlo_go/setup.py
@@ -79,7 +79,7 @@ class CustomBuildExt(build_ext):
 
 setuptools.setup(
     name=PACKAGE_NAME,
-    version="0.1.1",
+    version="0.1.2",
     author="Brett Jia",
     author_email="dev.bjia56@gmail.com",
     description="Go extensions for @scrypted/arlo",
