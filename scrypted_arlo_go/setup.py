6f7a4ab63def61f55e22a38983531497980915d6
Brett Jia <dev.bjia56@gmail.com>
2023-06-02 22:26:16 -0400

    add basic http client with ja3transport

--- scrypted_arlo_go/setup.py
+++ scrypted_arlo_go/setup.py
@@ -79,7 +79,7 @@ class CustomBuildExt(build_ext):
 
 setuptools.setup(
     name=PACKAGE_NAME,
-    version="0.1.2",
+    version="0.1.3",
     author="Brett Jia",
     author_email="dev.bjia56@gmail.com",
     description="Go extensions for @scrypted/arlo",
