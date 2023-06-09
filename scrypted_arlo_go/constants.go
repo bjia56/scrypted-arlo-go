83ec6076c04284d82ef6ae729977735281414c6c
Brett Jia <dev.bjia56@gmail.com>
2023-06-09 15:48:11 -0400

    update auth hosts

--- scrypted_arlo_go/constants.go
+++ scrypted_arlo_go/constants.go
@@ -2,4 +2,7 @@ package scrypted_arlo_go
 
 var BACKUP_AUTH_HOSTS = []string{
 	"MzQuMjU0LjIxNS4xOTQ=",
+	"NTIuMTguMTE4LjE=",
+	"MzQuMjQ5LjEyMi4xNQ==",
+	"NTIuMjE0Ljc3LjMx",
 }
--- scrypted_arlo_go/setup.py
+++ scrypted_arlo_go/setup.py
@@ -79,7 +79,7 @@ class CustomBuildExt(build_ext):
 
 setuptools.setup(
     name=PACKAGE_NAME,
-    version="0.1.4",
+    version="0.1.5",
     author="Brett Jia",
     author_email="dev.bjia56@gmail.com",
     description="Go extensions for @scrypted/arlo",
