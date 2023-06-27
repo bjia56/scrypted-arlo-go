274cb03e43e555137f8fcd7249e40c39139be656
Brett Jia <dev.bjia56@gmail.com>
2023-06-26 21:19:14 -0400

    Update constants.go

--- scrypted_arlo_go/constants.go
+++ scrypted_arlo_go/constants.go
@@ -1,8 +1,5 @@
 package scrypted_arlo_go
 
 var BACKUP_AUTH_HOSTS = []string{
-	"MzQuMjU0LjIxNS4xOTQ=",
-	"NTIuMTguMTE4LjE=",
-	"MzQuMjQ5LjEyMi4xNQ==",
-	"NTIuMjE0Ljc3LjMx",
+	"NTIuMjEyLjIwNS4xNDU=",
 }
