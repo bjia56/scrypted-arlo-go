a89de8c65b99ca2e53b7b145a853b0af0a3d91e8
Brett Jia <dev.bjia56@gmail.com>
2023-10-04 09:50:29 -0400

    build python 3.12 wheels

--- scrypted_arlo_go/builder/macos_build_wheels.sh
+++ scrypted_arlo_go/builder/macos_build_wheels.sh
@@ -29,7 +29,7 @@ build_wheel() (
     PY_MINOR=$1
     ln -sf /usr/local/opt/python@3.$PY_MINOR/bin/python3.$PY_MINOR  /usr/local/bin/python_for_build
     /usr/local/bin/python_for_build --version
-    /usr/local/bin/python_for_build -m pip install cibuildwheel==2.11.2 pybindgen
+    /usr/local/bin/python_for_build -m pip install cibuildwheel==2.16.2 pybindgen
     CIBW_BUILD="cp3$PY_MINOR-*" /usr/local/bin/python_for_build -m cibuildwheel --output-dir wheelhouse scrypted_arlo_go
 )
 
@@ -62,3 +62,8 @@ _start_group "Python 3.11"
 install_python 11
 build_wheel 11
 test_wheel 11
+
+_start_group "Python 3.12"
+install_python 12
+build_wheel 12
+test_wheel 12
