901c38630b65f59a9330b57d63c76b1f06c85e70
Brett Jia <dev.bjia56@gmail.com>
2023-11-15 23:02:42 -0500

    bump 0.6.1

--- scrypted_arlo_go/setup.py
+++ scrypted_arlo_go/setup.py
@@ -83,7 +83,7 @@ class CustomBuildExt(build_ext):
 
 setuptools.setup(
     name=PACKAGE_NAME,
-    version="0.6.0",
+    version="0.6.1",
     author="Brett Jia",
     author_email="dev.bjia56@gmail.com",
     description="Go extensions for @scrypted/arlo",
