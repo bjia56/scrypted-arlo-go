42ccc76b7dbee0cedad7ed3e4f57e79f11a51f18
Brett Jia <dev.bjia56@gmail.com>
2023-11-16 11:11:40 -0500

    force uninstall python

--- scrypted_arlo_go/builder/macos_build_wheels.sh
+++ scrypted_arlo_go/builder/macos_build_wheels.sh
@@ -27,7 +27,7 @@ install_python() (
 
 uninstall_python() (
     PY_MINOR=$1
-    brew uninstall --ignore-dependencies python@3.$PY_MINOR
+    brew uninstall --ignore-dependencies --force python@3.$PY_MINOR
 )
 
 build_wheel() (
