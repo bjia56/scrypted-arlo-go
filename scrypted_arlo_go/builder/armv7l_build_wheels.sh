0e50bafbf9062ed20426e9ab9fbc31c0441fb28c
Brett Jia <dev.bjia56@gmail.com>
2025-02-19 11:03:48 -0500

    use select_python

--- scrypted_arlo_go/builder/armv7l_build_wheels.sh
+++ scrypted_arlo_go/builder/armv7l_build_wheels.sh
@@ -8,14 +8,14 @@ build_wheel() (
     PY_VER=$1
     mkdir build$PY_VER
     cd build$PY_VER
-    pip$PY_VER wheel ..
+    pip3 wheel ..
 )
 
 test_wheel() (
     PY_VER=$1
     cd build$PY_VER
-    pip$PY_VER install *armv7l.whl
-    python$PY_VER -c "import scrypted_arlo_go; print(scrypted_arlo_go)"
+    pip3 install *armv7l.whl
+    python3 -c "import scrypted_arlo_go; print(scrypted_arlo_go)"
 )
 
 repair_wheel() (
@@ -24,6 +24,7 @@ repair_wheel() (
     auditwheel repair *armv7l.whl
 )
 
+select_python 3.$PYTHON3_VERSION
 build_wheel 3.$PYTHON3_VERSION
 repair_wheel 3.$PYTHON3_VERSION
 test_wheel 3.$PYTHON3_VERSION
